68bb27f14ae62f34d5f9b1deeea9d1e699090b11
Weifc <2418060010@qq.com>
2020-08-21 10:47:45 +0800

    QtQQ-自定义标题栏类设计2

--- titlebar.cpp
+++ titlebar.cpp
@@ -1,10 +1,71 @@
 #include "titlebar.h"
 
+#include <QHBoxLayout>
+
+#define BUTTON_HEIGHT 27 //按钮高度
+#define BUTTON_WIDTH 27  //按钮高度
+#define TITLE_HEIGHT 27  //标题栏高度
+
 TitleBar::TitleBar(QWidget *parent)
 	: QWidget(parent)
+	,m_isPressed(false)
+	,m_buttonType(MIN_MAN_BUTTON)
 {
+	initControl();
+	initConnections();
+	loadStyleSheet("Title");
 }
 
 TitleBar::~TitleBar()
 {
+}
+
+//初始化控件
+void TitleBar::initControl()
+{
+	m_pIcon = new QLabel(this);
+	m_pTitleContent = new QLabel(this);
+
+	m_pButtonMin = new QPushButton(this);
+	m_pButtonRestore = new QPushButton(this);
+	m_pButtonMax = new QPushButton(this);
+	m_pButtonClose = new QPushButton(this);
+
+	m_pButtonMin->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+	m_pButtonRestore->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+	m_pButtonMax->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+	m_pButtonClose->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+
+	//设置对象名
+	m_pTitleContent->setObjectName("TitleContent");
+	m_pButtonMin->setObjectName("ButtonMix");
+	m_pButtonRestore->setObjectName("ButtonRestore");
+	m_pButtonMax->setObjectName("ButtonMax");
+	m_pButtonClose->setObjectName("ButtonClose");
+
+	//设置布局
+	QHBoxLayout* mylayout = new QHBoxLayout(this);
+	mylayout->addWidget(m_pIcon);
+	mylayout->addWidget(m_pTitleContent);
+	mylayout->addWidget(m_pButtonMin);
+	mylayout->addWidget(m_pButtonRestore);
+	mylayout->addWidget(m_pButtonMax);
+	mylayout->addWidget(m_pButtonClose);
+
+	mylayout->setContentsMargins(5, 0, 0, 0); //设置布局的间隙
+	mylayout->setSpacing(0);                  //设置布局里部件间隙
+
+	m_pTitleContent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+	setFixedHeight(TITLE_HEIGHT);
+	setWindowFlags(Qt::FramelessWindowHint);
+
+}
+
+//信号与槽的连接
+void TitleBar::initConnections()
+{
+	connect(m_pButtonMin, SIGNAL(clicked()), this, SLOT(onButtonMinClicked()));
+	connect(m_pButtonRestore, SIGNAL(clicked()), this, SLOT(onButtonRestoreClicked()));
+	connect(m_pButtonMax, SIGNAL(clicked()), this, SLOT(onButtonMaxClicked()));
+	connect(m_pButtonClose, SIGNAL(clicked()), this, SLOT(onButtonCloseClicked()));
 }
