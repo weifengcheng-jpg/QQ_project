42aa8cbb3b089676c0630afb7023954544f3a400
Weifc <2418060010@qq.com>
2020-08-22 14:52:01 +0800

    QtQQ-窗基类设计上

---
+++ basicwindow.cpp
@@ -0,0 +1,68 @@
+#include "basicwindow.h"
+#include <QFile>
+
+BasicWindow::BasicWindow(QWidget *parent)
+	: QDialog(parent)
+{
+	setWindowFlags(Qt::FramelessWindowHint);
+	setAttribute(Qt::WA_TranslucentBackground, true);
+}
+
+BasicWindow::~BasicWindow()
+{
+}
+
+void BasicWindow::onSignalSkinChanged(const QColor& color)
+{
+	m_colorBackGround = color;
+	loadStyleSheet(m_styleName);
+}
+
+void BasicWindow::setTitleBarTitle(const QString& title, const QString& icon)
+{
+	_titleBar->setTitleIcon(icon);
+	_titleBar->setTitleContent(title);
+}
+
+void BasicWindow::initTitleBar(ButtonType buttontype)
+{
+	_titleBar = new TitleBar(this);
+	_titleBar->setButtonType(buttontype);
+	_titleBar->move(0, 0);
+
+	connect(_titleBar, SIGNAL(signalButtonMinClicked()), this, SLOT(onButtonMinClicked()));
+	connect(_titleBar, SIGNAL(signalButtonRestonreClicked()), this, SLOT(onButtonRestoreClicked()));
+	connect(_titleBar, SIGNAL(signalButtonMaxClicked()), this, SLOT(onButtonMaxClicked()));
+	connect(_titleBar, SIGNAL(signalButtonCloseClicked()), this, SLOT(onButtonCloseClicked()));
+}
+
+void BasicWindow::loadStyleSheet(const QString& sheetName)
+{
+	m_styleName == sheetName;
+	QFile file(":/Resouces/QSS/" + sheetName + ".css");
+	file.open(QFile::ReadOnly);
+
+	if (file.isOpen())
+	{
+		setStyleSheet("");
+		QString qsstyleSheet = QLatin1String(file.readAll());
+
+		//获取用户当前的皮肤RGB值
+		QString r = QString::number(m_colorBackGround.red());
+		QString g = QString::number(m_colorBackGround.green());
+		QString b = QString::number(m_colorBackGround.blue());
+
+		qsstyleSheet += QString("QWidget[titleskin=true]\
+								{background-color:rgb(%1,%2,%3):\
+								border-top-left-radius:4px:}\
+								QWidget[bottomskin=true]\
+								{border-top:lpx solid rgba(%1,%2,%3,100):\
+								background-color:rgba(%1,%2,%3,50):\
+								border-bottom-left-radius:4px:}")
+								.arg(r).arg(g).arg(b);
+
+		setStyleSheet(qsstyleSheet);
+	}
+
+	file.close();
+}
